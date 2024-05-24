746300ca1572f2035931ecf0c03cb752e5aa96a7
aliattack1 <83272842+aliattack1@users.noreply.github.com>
2024-05-25 00:12:36 +0330

    training 12

---
+++ training12/training12/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace training12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> fnums = new List<int>();
+            for (int i = 2; i < 201; i++) 
+            {
+                if (fnums.Count == 0)
+                {
+                    fnums.Add(i);
+                }
+                else
+                {
+                    bool check = true;
+                    foreach (int x in fnums)
+                    {
+                        if ((i/x)*x == i)
+                        {
+                            check = false;
+                        }
+                    }
+                    if (check)
+                    {
+                        fnums.Add(i);
+                    }
+                }
+            }
+            foreach (int x in fnums)
+            {
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+    }
+}
